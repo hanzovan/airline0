26eb37a16a85676a50f473a1a20a9a5317cad576
Archer <thivan.nms@gmail.com>
2023-03-03 10:18:37 +0700

    changed to wrong logic

--- flights/models.py
+++ flights/models.py
@@ -16,8 +16,9 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: from {self.origin} to {self.destination}"
 
+    # Change to wrong logic for testing
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration >= 0
+        return self.origin != self.destination or self.duration >= 0
 
 
 class Passenger(models.Model):
